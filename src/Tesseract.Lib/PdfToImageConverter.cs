bb1d10194625e7f5c514b3d31ec992bc12652b2e
Franco Lianza <lianza.fl@gmail.com>
2020-03-29 16:11:25 -0300

    Changed pdfToImageConverter implementation

--- src/Tesseract.Lib/PdfToImageConverter.cs
+++ src/Tesseract.Lib/PdfToImageConverter.cs
@@ -1,46 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using Spire.Pdf;
+using System;
+using System.Drawing;
 
 namespace Tesseract.Lib
 {
     public class PdfToImageConverter : IPdfToBImageConverter
     {
-        private readonly IProcessRunner processRunner;
-        public PdfToImageConverter(IProcessRunner processRunner)
-        {
-            this.processRunner = processRunner;
-        }
-
         public string Convert(byte[] pdfData)
         {
-            var pdfFilePath = $"tess_{Guid.NewGuid().ToString("N")}.pdf";
-            var imageFilePath = $"tess_{Guid.NewGuid().ToString("N")}.png";
-            File.WriteAllBytes(pdfFilePath, pdfData);
-
-            try
-            {
-                var processResult = this.processRunner.RunProcess("convert", new[] { "-density 300", $"{pdfFilePath}[0]", imageFilePath });
-                //var processResult = this.processRunner.RunProcess("gs", new[] { "-sDEVICE=png16m", $"-o {imageFilePath}", "-r300", $"-c \"30000000 setvmthreshold\"", pdfFilePath });
-
-                if (processResult.ExitCode != 0)
-                {
-                    throw new InvalidOperationException(processResult.Error);
-                }
-
-                if (!File.Exists(imageFilePath))
-                {
-                    imageFilePath = imageFilePath.Replace(".png", "-0.png");
-                }
-            }
-            finally
-            {
-                if (File.Exists(pdfFilePath))
-                {
-                    File.Delete(pdfFilePath);
-                }
-            }
-            
-            return imageFilePath;
+            var pdf = new PdfDocument(pdfData);
+            var img = pdf.SaveAsImage(0, 150, 150);
+            var imgFilePath = $"tess_{Guid.NewGuid().ToString("N")}.png";
+            img.Save(imgFilePath);
+            return imgFilePath;
         }
     }
 }
